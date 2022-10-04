f060674333bb56caae8f33e9b82bda7acd121d2f
Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>
2022-10-04 22:25:41 +0800

    fix(backend): alter column id from num. to primary id and auto increment in CommunityActivitieHelpTypes table.
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>

--- src/sqldb/Tables/CommunityActivitiesHelpTypes.sql
+++ src/sqldb/Tables/CommunityActivitiesHelpTypes.sql
@@ -6,7 +6,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[CommunityActivitiesHelpTypes](
-	[Id] [int] NOT NULL,
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,,
 	[CommunityActivityId] [int] NOT NULL,
 	[HelpTypeId] [int] NOT NULL,
 	[Details] [nchar](100) NOT NULL
