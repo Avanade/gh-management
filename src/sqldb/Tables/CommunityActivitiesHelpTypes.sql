72f9ceb8c532167b7cfc5c2465ee76a6b0962599
Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>
2022-10-10 16:28:05 +0800

    Update CommunityActivitiesHelpTypes.sql
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>

--- src/sqldb/Tables/CommunityActivitiesHelpTypes.sql
+++ src/sqldb/Tables/CommunityActivitiesHelpTypes.sql
@@ -1,7 +1,6 @@
 CREATE TABLE [dbo].[CommunityActivitiesHelpTypes](
-	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,,
+	[Id] [int] NOT NULL PRIMARY KEY IDENTITY,
 	[CommunityActivityId] [int] NOT NULL,
 	[HelpTypeId] [int] NOT NULL,
 	[Details] [nchar](100) NOT NULL
-) ON [PRIMARY]
-
+)
