f5e413102e4105577109c696bfb30619f2443316
Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>
2022-10-10 16:26:38 +0800

    Update HelpTypes.sql
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricodelacruz@users.noreply.github.com>

--- src/sqldb/Tables/HelpTypes.sql
+++ src/sqldb/Tables/HelpTypes.sql
@@ -1,12 +1,4 @@
-/****** Object:  Table [dbo].[HelpTypes]    Script Date: 15/07/2022 7:51:29 am ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[HelpTypes](
 	[Id] [int] NOT NULL,
 	[Name] [nchar](50) NOT NULL
 ) ON [PRIMARY]
-GO
