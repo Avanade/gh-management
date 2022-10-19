dfac9c7b5df58261f5a0283cc7c754e0d2584cb2
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2022-10-19 21:22:07 +0800

    feat: check on community details page
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

--- src/goapp/routes/pages/community/requestcommunity.go
+++ src/goapp/routes/pages/community/requestcommunity.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	session "main/pkg/session"
 	template "main/pkg/template"
 	"net/http"
 
@@ -11,8 +12,14 @@ func CommunityHandler(w http.ResponseWriter, r *http.Request) {
 	req := mux.Vars(r)
 	id := req["id"]
 
+	sessionaz, _ := session.Store.Get(r, "auth-session")
+	iprofile := sessionaz.Values["profile"]
+	profile := iprofile.(map[string]interface{})
+	username := profile["preferred_username"]
+
 	data := map[string]interface{}{
-		"Id": id,
+		"Id":    id,
+		"Email": username,
 	}
 
 	template.UseTemplate(&w, r, "/community/requestcommunity", data)
