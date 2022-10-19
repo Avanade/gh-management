7d360130c1191da0ef2d1c7063de14502795902c
Daryl Tejares <daryltejares@gmail.com>
2022-10-19 17:08:02 +0800

    Updated Search SP
    Signed-off-by: Daryl Tejares <daryltejares@gmail.com>

--- src/sqldb/StoredProcedures/PR_Search_communities_projects_users.sql
+++ src/sqldb/StoredProcedures/PR_Search_communities_projects_users.sql
@@ -7,36 +7,42 @@ CREATE PROCEDURE [dbo].[PR_Search_communities_projects_users]
 AS 
 
 SELECT
-	'Users' [Source],[Name],
-	 UserPrincipalName [Description],
-	 Users.GitHubId [Id]
-FROM [dbo].[Users]
-WHERE [Name] LIKE '%'+@searchText+'%'
-OR [UserPrincipalName] LIKE '%'+@searchText+'%'
-
-
-UNION
-
-SELECT 
-	'Projects' [Source], [Name],
-	CASE
-		WHEN [CreatedBy] IS NULL THEN [RepositorySource]
-		ELSE [RepositorySource] + ' - ' + [CreatedBy]
-	END [Description],
-	Projects.Id [ID]
-FROM [dbo].[Projects]
-WHERE [Name] LIKE '%'+@searchText+'%'
-OR [CreatedBy] LIKE '%'+@searchText+'%'
+		'Users' [Source],[Name],
+		UserPrincipalName [Description],
+		Users.GitHubId [Id]
+FROM	[dbo].[Users]
+WHERE	[Name] LIKE '%'+@searchText+'%'
+		OR [UserPrincipalName] LIKE '%'+@searchText+'%'
 
 UNION
 
 SELECT 
-	'Communities' [Source],[Name],
-	[Description],
-	Communities.[Id]
-FROM [dbo].[Communities]
-WHERE [Name] LIKE '%'+@searchText+'%'
-OR [Description] LIKE '%'+@searchText+'%'
+		'Repositories' [Source], [Name],
+			CASE
+				WHEN [CreatedBy] IS NULL THEN [RepositorySource]
+				ELSE [RepositorySource] + ' - ' + [CreatedBy]
+			END [Description],
+				Projects.Id [ID]
+FROM	[dbo].[Projects]
+WHERE	[Name] LIKE '%'+@searchText+'%'
+		OR [CreatedBy]
+		LIKE '%'+@searchText+'%'
+
+UNION
+
+SELECT 
+		'Communities' [Source],c.[Name],
+		[Description],
+		c.[Id]
+FROM	[dbo].[Communities] c
+  	INNER JOIN ApprovalStatus T ON c.ApprovalStatusId = T.Id
+WHERE	(c.[Name] LIKE '%'+@searchText+'%'
+		OR [Description] LIKE '%'+@searchText+'%' )
+
+		AND 
+		(c.ApprovalStatusId = 5 OR c.CreatedBy = 'dennis.delamida@accenture.com')
+				AND
+		c.ApprovalStatusId =5
 
 ORDER BY [Name]
 OFFSET @offSet ROWS
