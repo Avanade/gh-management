f56ead8837f220b4f386f201db72f027df6575e2
Jerrico Dela Cruz <jerrico.tan.delacruz@gmail.com>
2022-11-10 22:12:49 +0800

    Modified rows and tab buttons of markdown editor.
    Signed-off-by: Jerrico Dela Cruz <jerrico.tan.delacruz@gmail.com>

--- src/goapp/public/components/markdownEditor.js
+++ src/goapp/public/components/markdownEditor.js
@@ -19,27 +19,27 @@ const markdownEditor = ({
                 <nav x-show="!disabledTab">
                     <ul class="flex space-x-4">
                         <li>
-                            <a @click="activeTab = 'write'" :class="{ 'font-bold': activeTab === 'write' }"
-                                class="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded" href="#">
+                            <button @click="activeTab = 'write'" class="px-3 py-2 font-medium text-sm rounded-md"
+                                :class="activeTab === 'write' ? 'bg-[#fff2eb] text-[#FF5800]':'text-gray-500 hover:text-gray-700'">
                                 Write
-                            </a>
+                            </button>
                         </li>
                         <li>
-                            <a @click="activeTab = 'preview'" :class="{ 'font-bold': activeTab === 'preview' }"
-                                class="bg-amber-500 hover:bg-amber-700 text-white font-bold py-2 px-4 rounded" href="#">
+                            <button @click="activeTab = 'preview'" class="px-3 py-2 font-medium text-sm rounded-md"
+                                :class="activeTab === 'preview' ? 'bg-[#fff2eb] text-[#FF5800]':'text-gray-500 hover:text-gray-700'">
                                 Preview
-                            </a>
+                            </button>
                         </li>
                     </ul>
                 </nav>
                 <div class="py-6 w-full">
                     <div x-show="activeTab === 'write'">
                         <div>
-                            <textarea title="content" class="mt-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                            <textarea title="content" class="mt-1 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md h-44"
                                 x-model="body" x-on:input.change="preview"></textarea>
                         </div>
                     </div>
-                    <div x-show="activeTab === 'preview'" class="w-full border border-gray-300 rounded-md p-3">
+                    <div x-show="activeTab === 'preview'" class="w-full border border-gray-300 rounded-md p-3 h-44 overflow-y-auto">
                         <div x-html="markdown" class="preview prose max-w-none prose-img:rounded-md">
                         </div>
                     </div>
