4093f79f549b1f969b54f25f6e16b0f3918a7dd6
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-03-20 20:04:04 +0800

    refactor : Modified sql sp approval types total
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/sqldb/StoredProcedures/PR_ApprovalTypes_TotalCount.sql
+++ src/sqldb/StoredProcedures/PR_ApprovalTypes_TotalCount.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE [dbo].[PR_ApprovalTypes_TotalCount]
 AS
 BEGIN
-	SELECT COUNT(Id) AS 'Total' FROM [dbo].[ApprovalTypes]
+	SELECT COUNT(Id) AS 'Total' FROM [dbo].[ApprovalTypes] WHERE IsArchived = 0
 END
