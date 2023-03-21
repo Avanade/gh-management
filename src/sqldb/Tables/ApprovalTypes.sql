29ff9fd58ebb6e7b7cbad38cc96bd6d777220d56
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-03-21 23:36:04 +0800

    refactor : Modified MS SQL Approval Types table
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/sqldb/Tables/ApprovalTypes.sql
+++ src/sqldb/Tables/ApprovalTypes.sql
@@ -1,22 +1,28 @@
-﻿DELETE  
-    PA 
-FROM 
-    [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
-WHERE
-    AT.ApproverUserPrincipalName IS NULL
+﻿IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'ApprovalTypes'))
+BEGIN
+    DELETE  
+        PA
+    FROM 
+        [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
+    WHERE
+        AT.ApproverUserPrincipalName IS NULL
 
-DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
+    DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
+END
 
 CREATE TABLE [dbo].[ApprovalTypes]
 (
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Name] VARCHAR(50) NOT NULL,
     [ApproverUserPrincipalName] VARCHAR(100) NOT NULL,
     [IsArchived] BIT NOT NULL DEFAULT 0,
     [IsActive] BIT NOT NULL DEFAULT 1,
-    [Created] DATETIME NOT NULL DEFAULT getdate(), 
-    [CreatedBy] VARCHAR(100) NULL, 
-    [Modified] DATETIME NOT NULL DEFAULT getdate(), 
+    [Created] DATETIME NOT NULL DEFAULT getdate(),
+    [CreatedBy] VARCHAR(100) NULL,
+    [Modified] DATETIME NOT NULL DEFAULT getdate(),
     [ModifiedBy] VARCHAR(100) NULL
     CONSTRAINT FK_ApprovalTypes_Users FOREIGN KEY (ApproverUserPrincipalName) REFERENCES Users(UserPrincipalName)
 )
