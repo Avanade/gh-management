244fb0d04330d218e6ecfcf17c4f4064f328e355
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-03-21 18:29:33 +0800

    refactor : Added begin and end in approval types table sql
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/sqldb/Tables/ApprovalTypes.sql
+++ src/sqldb/Tables/ApprovalTypes.sql
@@ -1,26 +1,28 @@
-﻿DELETE  
-    PA 
-FROM 
-    [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
-WHERE
-	AT.ApproverUserPrincipalName IS NULL
+﻿BEGIN
+    DELETE  
+        PA 
+    FROM 
+        [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
+    WHERE
+        AT.ApproverUserPrincipalName IS NULL
 
-GO
+    GO
 
-DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
+    DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
 
-Go
+    Go
 
-CREATE TABLE [dbo].[ApprovalTypes]
-(
-    [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(50) NOT NULL,
-    [ApproverUserPrincipalName] VARCHAR(100) NOT NULL,
-    [IsArchived] BIT NOT NULL DEFAULT 0,
-    [IsActive] BIT NOT NULL DEFAULT 1,
-    [Created] DATETIME NOT NULL DEFAULT getdate(), 
-    [CreatedBy] VARCHAR(100) NULL, 
-    [Modified] DATETIME NOT NULL DEFAULT getdate(), 
-    [ModifiedBy] VARCHAR(100) NULL
-    CONSTRAINT FK_ApprovalTypes_Users FOREIGN KEY (ApproverUserPrincipalName) REFERENCES Users(UserPrincipalName)
-)
+    CREATE TABLE [dbo].[ApprovalTypes]
+    (
+        [Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+        [Name] VARCHAR(50) NOT NULL,
+        [ApproverUserPrincipalName] VARCHAR(100) NOT NULL,
+        [IsArchived] BIT NOT NULL DEFAULT 0,
+        [IsActive] BIT NOT NULL DEFAULT 1,
+        [Created] DATETIME NOT NULL DEFAULT getdate(), 
+        [CreatedBy] VARCHAR(100) NULL, 
+        [Modified] DATETIME NOT NULL DEFAULT getdate(), 
+        [ModifiedBy] VARCHAR(100) NULL
+        CONSTRAINT FK_ApprovalTypes_Users FOREIGN KEY (ApproverUserPrincipalName) REFERENCES Users(UserPrincipalName)
+    )
+END
