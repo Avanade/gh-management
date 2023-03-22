3d0e4a1386d23a7760f29b29e4c48782b0dd5213
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-03-22 16:07:39 +0800

    refactor: Modified ApprovalTypes SQL Added alter table after deleting item by null approvaluserprincipalname
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/sqldb/Tables/ApprovalTypes.sql
+++ src/sqldb/Tables/ApprovalTypes.sql
@@ -1,23 +1,8 @@
-﻿IF (EXISTS (SELECT * 
-                 FROM INFORMATION_SCHEMA.TABLES 
-                 WHERE TABLE_SCHEMA = 'dbo' 
-                 AND  TABLE_NAME = 'ApprovalTypes'))
-BEGIN
-    DELETE  
-        PA
-    FROM 
-        [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
-    WHERE
-        AT.ApproverUserPrincipalName IS NULL
-
-    DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
-END
-
-CREATE TABLE [dbo].[ApprovalTypes]
+﻿CREATE TABLE [dbo].[ApprovalTypes]
 (
     [Id] INT NOT NULL PRIMARY KEY IDENTITY,
     [Name] VARCHAR(50) NOT NULL,
-    [ApproverUserPrincipalName] VARCHAR(100) NOT NULL,
+    [ApproverUserPrincipalName] VARCHAR(100) NULL,
     [IsArchived] BIT NOT NULL DEFAULT 0,
     [IsActive] BIT NOT NULL DEFAULT 1,
     [Created] DATETIME NOT NULL DEFAULT getdate(),
@@ -26,3 +11,19 @@ CREATE TABLE [dbo].[ApprovalTypes]
     [ModifiedBy] VARCHAR(100) NULL
     CONSTRAINT FK_ApprovalTypes_Users FOREIGN KEY (ApproverUserPrincipalName) REFERENCES Users(UserPrincipalName)
 )
+GO
+
+DELETE  
+    PA
+FROM 
+    [dbo].[ProjectApprovals] PA INNER JOIN [dbo].[ApprovalTypes] AT ON PA.ApprovalTypeId = AT.Id
+WHERE
+    AT.ApproverUserPrincipalName IS NULL
+GO
+
+DELETE FROM [dbo].[ApprovalTypes] WHERE ApproverUserPrincipalName IS NULL
+GO
+
+ALTER TABLE [dbo].[ApprovalTypes]
+    ALTER COLUMN [ApproverUserPrincipalName] VARCHAR(100) NOT NULL
+GO
