1c8c8a24265eabce53cb44f1299dd7aa51b564d9
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2023-04-13 17:12:25 +0800

    Let admins make public repo to private
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

--- src/goapp/routes/pages/projects/projects.go
+++ src/goapp/routes/pages/projects/projects.go
@@ -1,12 +1,18 @@
 package routes
 
 import (
+	session "main/pkg/session"
 	template "main/pkg/template"
 	"net/http"
 )
 
 func Projects(w http.ResponseWriter, r *http.Request) {
-	template.UseTemplate(&w, r, "projects/projects", nil)
+	isAdmin, _ := session.IsUserAdmin(w, r)
+
+	data := map[string]interface{}{
+		"isAdmin": isAdmin,
+	}
+	template.UseTemplate(&w, r, "projects/projects", data)
 }
 
 func MakePublic(w http.ResponseWriter, r *http.Request) {
