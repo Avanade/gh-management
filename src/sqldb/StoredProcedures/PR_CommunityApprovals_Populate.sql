8ac53e9b582cf49ae7b5aafcf418e4bdbdc47e84
dennisdelamida <97105156+dennisdelamida@users.noreply.github.com>
2023-05-16 23:40:41 +0800

    bug fix
    Signed-off-by: dennisdelamida <97105156+dennisdelamida@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_CommunityApprovals_Populate.sql
+++ src/sqldb/StoredProcedures/PR_CommunityApprovals_Populate.sql
@@ -15,7 +15,7 @@ INSERT INTO CommunityApprovals
 SELECT @CommunityId, CAL.ApproverUserPrincipalName, 1, 'For Approval - ' + C.[Name], C.CreatedBy, C.CreatedBy
 FROM Communities C, CommunityApproversList CAL
 WHERE C.Id = @CommunityId
-
+AND CAL.Disabled =0
 UPDATE Communities SET ApprovalStatusId = 2, Modified = GETDATE() WHERE Id = @CommunityId
 
 EXEC PR_CommunityApprovals_Select_ById @CommunityId
