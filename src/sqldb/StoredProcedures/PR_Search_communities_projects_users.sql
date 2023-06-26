c7f32165478858229655115e3f401ac9b8f8a7d2
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-06-26 17:06:51 +0800

    refactor : Beautify the SP PR_Search_communities_projects_users
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/sqldb/StoredProcedures/PR_Search_communities_projects_users.sql
+++ src/sqldb/StoredProcedures/PR_Search_communities_projects_users.sql
@@ -1,71 +1,82 @@
 CREATE PROCEDURE [dbo].[PR_Search_communities_projects_users]
-
-@searchText VARCHAR (100),
-@offSet INT = 0,
-@rowCount INT = 0,
-@userprincipal VARCHAR (100) = null
+	@searchText VARCHAR (100),
+	@offSet INT = 0,
+	@rowCount INT = 0,
+	@userprincipal VARCHAR (100) = null
 AS 
-
-SELECT
-		'Users' [Source],[Name],
+	SELECT
+		'Users' [Source],
+		[Name],
 		UserPrincipalName [Description],
 		Users.GitHubId [Id]
-FROM	[dbo].[Users]
-WHERE	[Name] LIKE '%'+@searchText+'%'
-		OR [UserPrincipalName] LIKE '%'+@searchText+'%'
-
+	FROM 
+		[dbo].[Users]
+	WHERE	
+		[Name] LIKE '%'+@searchText+'%' OR 
+		[UserPrincipalName] LIKE '%'+@searchText+'%'
 UNION
-
-SELECT 
-'Repositories' [Source], [Name],
-			CONCAT(CASE
-				WHEN [CreatedBy] IS NULL THEN [RepositorySource]
-				ELSE [RepositorySource] + ' - ' + [CreatedBy]
-			END,'|',(
-				SELECT STRING_AGG(Topic, ',') FROM RepoTopics WHERE ProjectId=Id
-			)) [Description],
-				Id [ID]
-FROM (SELECT 
-	Name, RepositorySource, CreatedBy, Id
-FROM 
-	Projects AS P 
-LEFT JOIN 
-	RepoOwners AS RO ON RO.ProjectId = P.Id
-LEFT JOIN 
-	RepoTopics RT ON RT.ProjectId = P.Id
-WHERE	[Name] LIKE '%'+@searchText+'%'
-	OR 
-		RO.UserPrincipalName LIKE '%'+@searchText+'%'
-	OR 
-		RT.Topic LIKE '%'+@searchText+'%') AS Repository
-GROUP BY Name, CreatedBy, RepositorySource, Id
-
+	SELECT 
+		'Repositories' [Source], 
+		[Name],
+		CONCAT(
+			CASE
+			WHEN [CreatedBy] IS NULL THEN 
+				[RepositorySource]
+			ELSE 
+				[RepositorySource] + ' - ' + [CreatedBy]
+			END, '|', 
+			(
+				SELECT 
+					STRING_AGG(Topic, ',') 
+				FROM 
+					RepoTopics 
+				WHERE ProjectId=Id
+			)
+		) [Description],
+		Id [ID]
+	FROM (
+		SELECT 
+			Name, 
+			RepositorySource, 
+			CreatedBy, 
+			Id
+		FROM 
+			Projects AS P 
+		LEFT JOIN 
+			RepoOwners AS RO ON RO.ProjectId = P.Id
+		LEFT JOIN 
+			RepoTopics RT ON RT.ProjectId = P.Id
+		WHERE	
+			[Name] LIKE '%'+@searchText+'%' OR 
+			RO.UserPrincipalName LIKE '%'+@searchText+'%' OR 
+			RT.Topic LIKE '%'+@searchText+'%'
+	) AS Repository
+	GROUP BY Name, CreatedBy, RepositorySource, Id
 UNION
-
-SELECT 
-		'Communities' [Source],c.[Name],
+	SELECT 
+		'Communities' [Source],
+		c.[Name],
 		[Description],
 		c.[Id]
-FROM	[dbo].[Communities] c
-	LEFT JOIN ApprovalStatus T ON c.ApprovalStatusId = T.Id
-WHERE	(
-			(
-				c.[Name] LIKE '%'+@searchText+'%'
-				OR [Description] LIKE '%'+@searchText+'%' 
-			)
-
-			AND c.ApprovalStatusId = 5
-		)
-		OR
+	FROM	
+		[dbo].[Communities] c
+	LEFT JOIN 
+		ApprovalStatus T ON c.ApprovalStatusId = T.Id
+	WHERE 
 		(
 			(
-				c.[Name] LIKE '%'+@searchText+'%'
-				OR [Description] LIKE '%'+@searchText+'%' 
-			)
-
-			AND c.CreatedBy = @userprincipal
+				c.[Name] LIKE '%'+@searchText+'%' OR 
+				[Description] LIKE '%'+@searchText+'%' 
+			) AND 
+			c.ApprovalStatusId = 5
+		) OR
+		(
+			(
+				c.[Name] LIKE '%'+@searchText+'%' OR 
+				[Description] LIKE '%'+@searchText+'%' 
+			) AND 
+			c.CreatedBy = @userprincipal
 		)
-	
 ORDER BY [Name]
 OFFSET @offSet ROWS
 FETCH NEXT @rowCount ROWS ONLY
