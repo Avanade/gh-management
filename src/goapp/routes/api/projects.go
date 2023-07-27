0bceb6b776dfc5d70d7b0ad52d3170be1ee7430d
Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>
2023-07-27 22:54:01 +0800

    feat: Add & Update ECATT ID of User repository
    Signed-off-by: Jerrico Dela Cruz <jerrico.dela.cruz@accenture.com>

--- src/goapp/routes/api/projects.go
+++ src/goapp/routes/api/projects.go
@@ -194,6 +194,34 @@ func UpdateRepositoryById(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func UpdateRepositoryEcattIdById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	sessionaz, _ := session.Store.Get(r, "auth-session")
+	iprofile := sessionaz.Values["profile"]
+	profile := iprofile.(map[string]interface{})
+	username := profile["preferred_username"]
+
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var body RepoDto
+	err = json.NewDecoder(r.Body).Decode(&body)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	db.UpdateProjectEcattIdById(id, body.ECATTID, username.(string))
+
+	w.WriteHeader(http.StatusOK)
+}
+
 func GetUserProjects(w http.ResponseWriter, r *http.Request) {
 	// Get email address of the user
 	sessionaz, _ := session.Store.Get(r, "auth-session")
