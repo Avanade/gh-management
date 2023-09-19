9aee903820147b086bc93292ebed0ed37aa75c14
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2023-09-19 19:38:42 +0800

    WIP : Created SP for ProjectApprovalsand ApprovalRequestApprovers Populate
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_ProjectApprovals_ApprovalRequestApprovers_Populate.sql
+++ src/sqldb/StoredProcedures/PR_ProjectApprovals_ApprovalRequestApprovers_Populate.sql
@@ -1,9 +1,46 @@
-CREATE PROCEDURE [dbo].[PR_ProjectsApproval_Populate]
+CREATE PROCEDURE [dbo].[PR_ProjectApprovals_ApprovalRequestApprovers_Populate]
 (
     @ProjectId INT,
 	@RequestedBy VARCHAR(100)
 )
 AS
 BEGIN
-    
+    INSERT INTO ProjectApprovals
+    (
+		ProjectId,
+		ApprovalTypeId,
+		-- ApproverUserPrincipalName,
+		ApprovalStatusId,
+		ApprovalDescription,
+		CreatedBy
+	)
+    SELECT 
+        @ProjectId, 
+        T.Id, 
+        -- T.ApproverUserPrincipalName, 
+        1, 
+        'For Review - ' + T.[Name], 
+        @RequestedBy
+    FROM 
+        ApprovalTypes AS T
+    WHERE 
+        T.IsActive = 1 AND 
+        T.IsArchived = 0
+
+    INSERT INTO ApprovalRequestApprovers
+    (
+        ApprovalRequestId,
+        ApproverEmail
+    )
+    SELECT
+        PA.Id,
+        A.ApproverEmail
+    FROM
+        ProjectApprovals AS PA 
+        INNER JOIN
+        ApprovalTypes AS T ON PA.ApprovalTypeId = T.Id
+        INNER JOIN 
+        Approvers AS A ON T.Id = A.ApprovalTypeId
+    WHERE
+        PA.ProjectId = @ProjectId
 END
