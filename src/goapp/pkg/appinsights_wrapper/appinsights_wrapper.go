b707dacd98543c9fac1a6c62e2a4e22b5e9d287b
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2023-12-12 19:11:01 +0800

    feat : Added log file name and line no. in appinsights wrapper
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/goapp/pkg/appinsights_wrapper/appinsights_wrapper.go
+++ src/goapp/pkg/appinsights_wrapper/appinsights_wrapper.go
@@ -3,9 +3,11 @@ package appinsights_wrapper
 import (
 	"fmt"
 	"log"
+	"runtime"
 	"time"
 
 	"github.com/microsoft/ApplicationInsights-Go/appinsights"
+	"github.com/microsoft/ApplicationInsights-Go/appinsights/contracts"
 )
 
 var (
@@ -32,15 +34,56 @@ func NewClient() *TelemetryClient {
 	}
 }
 
-func (c *TelemetryClient) StartOperation(name string) {
-	c.Context().Tags.Operation().SetId(newUUID().String())
-	c.Context().Tags.Operation().SetName(name)
-	fmt.Printf("\nSTART OPERATION | ID:%s\n", c.Context().Tags.Operation().GetId())
+func (tc *TelemetryClient) StartOperation(name string) {
+	tc.Context().Tags.Operation().SetId(newUUID().String())
+	tc.Context().Tags.Operation().SetName(name)
+	fmt.Printf("\nSTART OPERATION | ID:%s\n", tc.Context().Tags.Operation().GetId())
 }
 
-func (c *TelemetryClient) EndOperation() {
-	fmt.Printf("\nEND OPERATION | ID:%s\n", c.Context().Tags.Operation().GetId())
-	for k := range c.Context().Tags.Operation() {
-		delete(c.Context().Tags.Operation(), k)
+func (tc *TelemetryClient) EndOperation() {
+	fmt.Printf("\nEND OPERATION | ID:%s\n", tc.Context().Tags.Operation().GetId())
+	for k := range tc.Context().Tags.Operation() {
+		delete(tc.Context().Tags.Operation(), k)
 	}
+}
+
+func (tc *TelemetryClient) Log(telemetry appinsights.Telemetry) {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		file = "???"
+		line = 0
+	}
+
+	tc.Context().CommonProperties["file"] = file
+	tc.Context().CommonProperties["line"] = fmt.Sprint(line)
+
+	tc.Track(telemetry)
+}
+
+func (tc *TelemetryClient) LogEvent(name string) {
+	tc.Log(appinsights.NewEventTelemetry(name))
+}
+
+func (tc *TelemetryClient) LogMetric(name string, value float64) {
+	tc.Log(appinsights.NewMetricTelemetry(name, value))
+}
+
+func (tc *TelemetryClient) LogTrace(message string, severity contracts.SeverityLevel) {
+	tc.Log(appinsights.NewTraceTelemetry(message, severity))
+}
+
+func (tc *TelemetryClient) LogRequest(method, url string, duration time.Duration, responseCode string) {
+	tc.Log(appinsights.NewRequestTelemetry(method, url, duration, responseCode))
+}
+
+func (tc *TelemetryClient) LogRemoteDependency(name, dependencyType, target string, success bool) {
+	tc.Log(appinsights.NewRemoteDependencyTelemetry(name, dependencyType, target, success))
+}
+
+func (tc *TelemetryClient) LogAvailability(name string, duration time.Duration, success bool) {
+	tc.Log(appinsights.NewAvailabilityTelemetry(name, duration, success))
+}
+
+func (tc *TelemetryClient) LogException(err interface{}) {
+	tc.Log(appinsights.NewExceptionTelemetry(err))
 }
