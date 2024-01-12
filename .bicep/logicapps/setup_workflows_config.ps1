0bd825c7ebd8c34e2a0131d7a4f99efdb3cdd4be
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-01-12 17:55:17 +0800

    test
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- .bicep/logicapps/setup_workflows_config.ps1
+++ .bicep/logicapps/setup_workflows_config.ps1
@@ -15,17 +15,7 @@ function New-WokflowsConfig {
     foreach ($workflowName in $workflowNamesArray) {
         $workflowNamesDictionary[$workflowName] = $false
     }
-
-    $deploymentParameters = {
-        "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#",
-        "contentVersion": "1.0.0.0",
-        "parameters": {
-            "appSettings": {
-                "value": $workflowNamesDictionary
-            }
-        }
-    }
-
+    
     $result = $deploymentParameters | ConvertTo-Json
 
     return $result
