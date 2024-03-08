802e07b36940fb00a488e356078f99a6bdf4473f
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2024-03-08 19:14:43 +0800

    fix build error
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

--- src/sqldb/Tables/OrganizationApprovalRequests.sql
+++ src/sqldb/Tables/OrganizationApprovalRequests.sql
@@ -3,6 +3,5 @@ CREATE TABLE [dbo].[OrganizationApprovalRequests]
 	[OrganizationId] INT NOT NULL, 
     [RequestId] INT NOT NULL,
     PRIMARY KEY (OrganizationId, RequestId),
-    CONSTRAINT [FK_OrganizationApprovalRequests_Organizations] FOREIGN KEY (OrganizationId) REFERENCES Organizations(Id),
-    CONSTRAINT [FK_OrganizationApprovalRequests_CommunityApprovals] FOREIGN KEY (RequestId) REFERENCES CommmunityApprovals(Id)
+    CONSTRAINT [FK_OrganizationApprovalRequests_Organizations] FOREIGN KEY (OrganizationId) REFERENCES Organizations(Id)
 )
