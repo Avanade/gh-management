324e6183b18ee0992a6568c0a0859a43fb0af285
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2024-03-11 22:24:06 +0800

    feat: database updates
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

--- src/sqldb/Tables/Organizations.sql
+++ src/sqldb/Tables/Organizations.sql
@@ -5,6 +5,7 @@ CREATE TABLE [dbo].[Organizations]
     [ClientName] VARCHAR(100) NOT NULL,
     [ProjectName] VARCHAR(100) NOT NULL,
     [WBS] VARCHAR(50) NOT NULL,
+    [ApprovalStatusId] INT NOT NULL DEFAULT 1,
     [Created] DATETIME NOT NULL DEFAULT getdate(), 
     [CreatedBy] VARCHAR(100) NULL, 
     CONSTRAINT [FK_Organizations_RegionalOrganizations] FOREIGN KEY (Region) REFERENCES RegionalOrganizations(Id)
