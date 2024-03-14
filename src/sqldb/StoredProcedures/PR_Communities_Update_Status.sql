a2336c47c547e33a702b87175d153916b47e2d6f
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2024-03-14 17:42:17 +0800

    fix: Update status
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_Communities_Update_Status.sql
+++ src/sqldb/StoredProcedures/PR_Communities_Update_Status.sql
@@ -3,9 +3,19 @@ CREATE PROCEDURE [dbo].[PR_Communities_Update_Status]
 
 AS
 
-IF EXISTS (SELECT ApprovalStatusId FROM CommunityApprovals WHERE CommunityId = @CommunityId AND ApprovalStatusId <> 1) -- IF THERE IS A RESPONSE
+IF EXISTS (
+	SELECT CA.ApprovalStatusId 
+	FROM CommunityApprovalRequests CAR
+	LEFT JOIN CommunityApprovals CA ON CAR.RequestId = CA.Id
+	WHERE CAR.CommunityId = @CommunityId AND CA.ApprovalStatusId <> 1
+) -- IF THERE IS A RESPONSE
 BEGIN
 	UPDATE Communities
-	SET ApprovalStatusId = (SELECT TOP 1 ApprovalStatusId FROM CommunityApprovals WHERE CommunityId = @CommunityId AND ApprovalDate IS NOT NULL ORDER BY ApprovalDate)
+	SET ApprovalStatusId = (
+		SELECT TOP 1 CA.ApprovalStatusId 
+		FROM CommunityApprovalRequests CAR
+		LEFT JOIN CommunityApprovals CA ON CAR.RequestId = CA.Id
+		WHERE CAR.CommunityId = @CommunityId AND CA.ApprovalDate IS NOT NULL ORDER BY ApprovalDate
+		)
 	WHERE Id = @CommunityId
 END
