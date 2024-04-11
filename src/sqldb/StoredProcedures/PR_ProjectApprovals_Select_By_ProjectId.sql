f419eca0165d160ea83896c0745c3e05d95f9487
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-04-11 22:55:59 +0800

    fix: Get all newest approvers
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_ProjectApprovals_Select_By_ProjectId.sql
+++ src/sqldb/StoredProcedures/PR_ProjectApprovals_Select_By_ProjectId.sql
@@ -1,23 +1,23 @@
-CREATE PROCEDURE [dbo].[PR_ProjectApprovals_Select_By_ProjectId]
+ALTER PROCEDURE [dbo].[PR_ProjectApprovals_Select_By_ProjectId]
   (
   @Id INT
 )
 AS
 BEGIN
-
   SELECT
-    PA.Id, PA.ProjectId,
-    P.[Name] [ProjectName],
-    PA.ApprovalTypeId, T.[Name] ApprovalType,
-    PA.ApproverUserPrincipalName,
-    PA.ApprovalDescription,
-    S.Name [RequestStatus],
-    PA.[ApprovalDate], PA.[ApprovalRemarks]
-  FROM
-    ProjectApprovals PA
-    INNER JOIN Projects P ON PA.ProjectId = P.Id
-    INNER JOIN ApprovalTypes T ON PA.ApprovalTypeId = T.Id
-    INNER JOIN ApprovalStatus S ON S.Id = PA.ApprovalStatusId
-  WHERE  
-    PA.[ProjectId] = @Id
+      PA.Id, PA.ProjectId,
+      P.[Name] [ProjectName],
+      PA.ApprovalTypeId, T.[Name] ApprovalType,
+      PA.ApproverUserPrincipalName,
+      PA.ApprovalDescription,
+      S.Name [RequestStatus],
+      PA.[ApprovalDate], PA.[ApprovalRemarks]
+    FROM
+      ProjectApprovals PA
+      INNER JOIN Projects P ON PA.ProjectId = P.Id
+      INNER JOIN ApprovalTypes T ON PA.ApprovalTypeId = T.Id
+      INNER JOIN ApprovalStatus S ON S.Id = PA.ApprovalStatusId
+    WHERE
+      PA.Created = (SELECT TOP(1) Created FROM ProjectApprovals WHERE ProjectId = @Id ORDER BY Created DESC)
+    ORDER BY PA.Created DESC
 END
