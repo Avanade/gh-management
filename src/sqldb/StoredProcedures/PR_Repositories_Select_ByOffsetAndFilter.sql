d1efc9db336b63fb97c697263aa47c62c5a156a0
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-05-13 19:59:25 +0800

    feat: Best match
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_Repositories_Select_ByOffsetAndFilter.sql
+++ src/sqldb/StoredProcedures/PR_Repositories_Select_ByOffsetAndFilter.sql
@@ -4,29 +4,42 @@ CREATE PROCEDURE [dbo].[PR_Repositories_Select_ByOffsetAndFilter](
 )
 AS
 BEGIN
-    SET NOCOUNT ON
-SELECT DISTINCT [p].[Id],
-        [p].[Name],
-        [P].[AssetCode],
-        [p].[ECATTID],
-        [p].[Organization],
-        [p].[Description],
-        [p].[IsArchived],
-        [p].[Created],
-        [p].[RepositorySource],
-        [p].[TFSProjectReference],
-        [v].[Name] AS "Visibility",
-		    [p].[CoOwner],
-        [p].[Createdby],
-        (SELECT STRING_AGG(r.Topic, ',') FROM dbo.RepoTopics AS r WHERE r.ProjectId=p.Id) AS "Topics"
-	  FROM [dbo].[Projects] AS p
-	  LEFT JOIN [dbo].[Visibility] AS v ON p.VisibilityId = v.Id
-    LEFT JOIN [dbo].[RepoTopics] AS rt ON rt.ProjectId = p.Id
-    WHERE
-		p.Name LIKE '%'+@search+'%' OR
-		rt.Topic LIKE '%'+@search+'%'
-    ORDER BY
-		[p].[Created] DESC
-	  OFFSET @Offset ROWS 
-	  FETCH NEXT 15 ROWS ONLY
+  SET NOCOUNT ON
+  SELECT  [p].[Id],
+          [p].[Name],
+          [P].[AssetCode],
+          [p].[ECATTID],
+          [p].[Organization],
+          [p].[Description],
+          [p].[IsArchived],
+          [p].[Created],
+          [p].[RepositorySource],
+          [p].[TFSProjectReference],
+          [v].[Name] AS "Visibility",
+          [p].[CoOwner],
+          [p].[Createdby],
+          (SELECT STRING_AGG(r.Topic, ',') FROM dbo.RepoTopics AS r WHERE r.ProjectId=p.Id) AS "Topics",
+          COUNT(*) AS Score
+      FROM [dbo].[Projects] AS p
+      LEFT JOIN [dbo].[Visibility] AS v ON p.VisibilityId = v.Id
+      LEFT JOIN [dbo].[RepoTopics] AS rt ON rt.ProjectId = p.Id
+      INNER JOIN STRING_SPLIT(@Search, ' ') AS ss ON (p.Name LIKE '%'+ss.[value]+'%' OR rt.Topic LIKE '%'+ss.[value]+'%')
+      GROUP BY [p].[Id],
+          [p].[Name],
+          [P].[AssetCode],
+          [p].[ECATTID],
+          [p].[Organization],
+          [p].[Description],
+          [p].[IsArchived],
+          [p].[Created],
+          [p].[RepositorySource],
+          [p].[TFSProjectReference],
+          [v].[Name],
+          [p].[CoOwner],
+          [p].[Createdby]
+      ORDER BY
+      Score DESC,
+      [p].[Created] DESC
+    OFFSET @Offset ROWS 
+    FETCH NEXT 15 ROWS ONLY
 END
--- src/sqldb/StoredProcedures/PR_Repositories_TotalCount_BySearchTerm.sql
+++ src/sqldb/StoredProcedures/PR_Repositories_TotalCount_BySearchTerm.sql
@@ -4,5 +4,12 @@ CREATE PROCEDURE [dbo].[PR_Repositories_TotalCount_BySearchTerm] (
 AS
 BEGIN
     SET NOCOUNT ON
-	SELECT COUNT(Id) AS Total FROM Projects WHERE Name LIKE '%'+@search+'%'
+	SELECT COUNT(*) AS Total FROM (
+		SELECT COUNT(Id) AS Total FROM Projects AS p
+		LEFT JOIN [dbo].[RepoTopics] AS rt ON rt.ProjectId = p.Id
+		INNER JOIN STRING_SPLIT(@search, ' ') AS ss ON (
+			p.Name LIKE '%'+ss.[value]+'%' OR rt.Topic LIKE '%'+ss.[value]+'%'
+		)
+		GROUP BY Id
+	) AS Total
 END
