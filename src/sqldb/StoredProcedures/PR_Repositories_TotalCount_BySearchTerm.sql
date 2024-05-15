f4662748f84198d5af568f7d54469d453bc49a73
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-05-15 17:38:45 +0800

    feat: Add topic filter
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/StoredProcedures/PR_Repositories_TotalCount_BySearchTerm.sql
+++ src/sqldb/StoredProcedures/PR_Repositories_TotalCount_BySearchTerm.sql
@@ -1,5 +1,7 @@
 CREATE PROCEDURE [dbo].[PR_Repositories_TotalCount_BySearchTerm] (
-	@search VARCHAR(50) = ''
+	@search VARCHAR(50) = '',
+	@Filter VARCHAR(MAX) = '',
+  	@FilterType TINYINT = 0
 )
 AS
 BEGIN
@@ -10,6 +12,12 @@ BEGIN
 		INNER JOIN STRING_SPLIT(@search, ' ') AS ss ON (
 			p.Name LIKE '%'+ss.[value]+'%' OR rt.Topic LIKE '%'+ss.[value]+'%'
 		)
+		WHERE 
+          @FilterType = 0
+        OR
+          (rt.Topic IN (SELECT value FROM STRING_SPLIT(@Filter, ',')) AND @FilterType = 1)
+        OR
+          (rt.Topic IS NULL AND @FilterType = 2)
 		GROUP BY Id
 	) AS Total
 END
