928d5c651cdb728afa2c5611daebb9115525cba2
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-05-29 19:26:44 +0800

    refactor: Reformat syntax of data initialization in postdeployment
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/Script.PostDeployment.sql
+++ src/sqldb/Script.PostDeployment.sql
@@ -1,89 +1,58 @@
 -- This file contains SQL statements that will be executed after the build script.
+
 /* INITIAL DATA FOR APPROVAL STATUS */
-SET IDENTITY_INSERT ApprovalStatus ON IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 1
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (1, 'New') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 2
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (2, 'InReview') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 3
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (3, 'Rejected') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 4
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (4, 'NonCompliant') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 5
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (5, 'Approved') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 6
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (6, 'Retired') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalStatus]
-        WHERE [Id] = 7
-    )
-INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name])
-VALUES (7, 'Archived')
+SET IDENTITY_INSERT ApprovalStatus ON 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 1)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (1, 'New') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 2)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (2, 'InReview') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 3)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (3, 'Rejected') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 4)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (4, 'NonCompliant') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 5)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (5, 'Approved') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 6)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (6, 'Retired') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalStatus] WHERE [Id] = 7)
+        INSERT INTO [dbo].[ApprovalStatus] ([Id], [Name]) VALUES (7, 'Archived')
 SET IDENTITY_INSERT ApprovalStatus OFF
-    /* INITIAL DATA FOR APPROVAL TYPES */
-SET IDENTITY_INSERT ApprovalTypes ON IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalTypes]
-        WHERE [Id] = 1
-    )
-INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name])
-VALUES (1, 'Intellectual Property') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalTypes]
-        WHERE [Id] = 2
-    )
-INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name])
-VALUES (2, 'Legal') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[ApprovalTypes]
-        WHERE [Id] = 3
-    )
-INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name])
-VALUES (3, 'Security')
-SET IDENTITY_INSERT ApprovalTypes OFF
-SET IDENTITY_INSERT Visibility ON IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[Visibility]
-        WHERE [Id] = 1
-    )
-INSERT INTO [dbo].[Visibility] ([Id], [Name])
-VALUES (1, 'Private') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[Visibility]
-        WHERE [Id] = 2
-    )
-INSERT INTO [dbo].[Visibility] ([Id], [Name])
-VALUES (2, 'Internal') IF NOT EXISTS (
-        SELECT [Id]
-        FROM [dbo].[Visibility]
-        WHERE [Id] = 3
-    )
-INSERT INTO [dbo].[Visibility] ([Id], [Name])
-VALUES (3, 'Public')
+
+/* INITIAL DATA FOR APPROVAL TYPES */
+IF (EXISTS (SELECT * 
+                 FROM INFORMATION_SCHEMA.TABLES 
+                 WHERE TABLE_SCHEMA = 'dbo' 
+                 AND  TABLE_NAME = 'RepositoryApprovalType'))
+    BEGIN
+        SET IDENTITY_INSERT RepositoryApprovalType ON
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[RepositoryApprovalType] WHERE [Id] = 1)
+                INSERT INTO [dbo].[RepositoryApprovalType] ([Id], [Name]) VALUES (1, 'Intellectual Property') 
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[RepositoryApprovalType] WHERE [Id] = 2)
+                INSERT INTO [dbo].[RepositoryApprovalType] ([Id], [Name]) VALUES (2, 'Legal') 
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[RepositoryApprovalType] WHERE [Id] = 3)
+                INSERT INTO [dbo].[RepositoryApprovalType] ([Id], [Name]) VALUES (3, 'Security')
+        SET IDENTITY_INSERT RepositoryApprovalType OFF
+    END
+ELSE
+    BEGIN
+        SET IDENTITY_INSERT ApprovalTypes ON
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalTypes] WHERE [Id] = 1)
+                INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name]) VALUES (1, 'Intellectual Property') 
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalTypes] WHERE [Id] = 2)
+                INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name]) VALUES (2, 'Legal') 
+            IF NOT EXISTS (SELECT [Id] FROM [dbo].[ApprovalTypes] WHERE [Id] = 3)
+                INSERT INTO [dbo].[ApprovalTypes] ([Id], [Name]) VALUES (3, 'Security')
+        SET IDENTITY_INSERT ApprovalTypes OFF
+    END
+
+/* INITIAL DATA FOR VISIBILITY */
+SET IDENTITY_INSERT Visibility ON 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[Visibility] WHERE [Id] = 1)
+        INSERT INTO [dbo].[Visibility] ([Id], [Name]) VALUES (1, 'Private') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[Visibility] WHERE [Id] = 2)
+        INSERT INTO [dbo].[Visibility] ([Id], [Name]) VALUES (2, 'Internal') 
+    IF NOT EXISTS (SELECT [Id] FROM [dbo].[Visibility] WHERE [Id] = 3)
+        INSERT INTO [dbo].[Visibility] ([Id], [Name]) VALUES (3, 'Public')
 SET IDENTITY_INSERT Visibility OFF
 
 /* RENAME ALL TABLES */
