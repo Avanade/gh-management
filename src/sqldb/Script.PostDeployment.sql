fcb543e85a74102e3988c27d8061a896fb68c46c
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-05-29 18:20:40 +0800

    refactor: Rename all tables via postdeployment
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/sqldb/Script.PostDeployment.sql
+++ src/sqldb/Script.PostDeployment.sql
@@ -85,3 +85,110 @@ VALUES (2, 'Internal') IF NOT EXISTS (
 INSERT INTO [dbo].[Visibility] ([Id], [Name])
 VALUES (3, 'Public')
 SET IDENTITY_INSERT Visibility OFF
+
+/* RENAME ALL TABLES */
+
+/* ActivityTypes > ActivityType */
+EXEC sp_rename 'dbo.ActivityTypes', 'ActivityType'
+
+/* ApprovalRequestApprovers > ApprovalRequestApprover */
+EXEC sp_rename 'dbo.ApprovalRequestApprovers', 'ApprovalRequestApprover'
+
+/* ApprovalStatus > ApprovalStatus */
+
+
+/* ApprovalTypes > RepositoryApprovalType */
+EXEC sp_rename 'dbo.ApprovalTypes', 'RepositoryApprovalType'
+
+/* Approvers > RepositoryApprover */
+EXEC sp_rename 'dbo.Approvers', 'RepositoryApprover'
+
+/* Category > GuidanceCategory */
+EXEC sp_rename 'dbo.Category', 'GuidanceCategory'
+
+/* CategoryArticles > GuidanceCategoryArticle */
+EXEC sp_rename 'dbo.CategoryArticles', 'GuidanceCategoryArticle'
+
+/* Communities > Community */
+EXEC sp_rename 'dbo.Communities', 'Community'
+
+/* CommunityActivities > CommunityActivity */
+EXEC sp_rename 'dbo.CommunityActivities', 'CommunityActivity'
+
+/* CommunityActivitiesContributionAreas > CommunityActivityContributionArea */
+EXEC sp_rename 'dbo.CommunityActivitiesContributionAreas', 'CommunityActivityContributionArea'
+
+/* CommunityActivitiesHelpTypes > CommunityActivityHelpType */
+EXEC sp_rename 'dbo.CommunityActivitiesHelpTypes', 'CommunityActivityHelpType'
+
+/* CommunityApprovalRequests > CommunityApprovalRequest */
+EXEC sp_rename 'dbo.CommunityApprovalRequests', 'CommunityApprovalRequest'
+
+/* CommunityApprovals > ApprovalRequest */
+EXEC sp_rename 'dbo.CommunityApprovals', 'ApprovalRequest'
+
+/* CommunityApproversList > CommunityApprover */
+EXEC sp_rename 'dbo.CommunityApproversList', 'CommunityApprover'
+
+/* CommunityMembers > CommunityMember */
+EXEC sp_rename 'dbo.CommunityMembers', 'CommunityMember'
+
+/* CommunitySponsors > CommunitySponsor */
+EXEC sp_rename 'dbo.CommunitySponsors', 'CommunitySponsor'
+
+/* CommunityTags > CommunityTag */
+EXEC sp_rename 'dbo.CommunityTags', 'CommunityTag'
+
+/* ContributionAreas > ContributionArea */
+EXEC sp_rename 'dbo.ContributionAreas', 'ContributionArea'
+
+/* ExternalLinks > ExternalLink */
+EXEC sp_rename 'dbo.ExternalLinks', 'ExternalLink'
+
+/* GitHubAccess > GitHubAccessDirectoryGroup */
+EXEC sp_rename 'dbo.GitHubAccess', 'GitHubAccessDirectoryGroup'
+
+/* GitHubCopilot > GitHubCopilot */
+
+
+/* GitHubCopilotApprovalRequests > GitHubCopilotApprovalRequest */
+EXEC sp_rename 'dbo.GitHubCopilotApprovalRequests', 'GitHubCopilotApprovalRequest'
+
+/* OrganizationAccess > OrganizationAccess */
+
+
+/* OrganizationAccessApprovalRequests > OrganizationAccessApprovalRequest */
+EXEC sp_rename 'dbo.OrganizationAccessApprovalRequests', 'OrganizationAccessApprovalRequest'
+
+/* OrganizationApprovalRequests > OrganizationApprovalRequest */
+EXEC sp_rename 'dbo.OrganizationApprovalRequests', 'OrganizationApprovalRequest'
+
+/* Organizations > Organization */
+EXEC sp_rename 'dbo.Organizations', 'Organization'
+
+/* OSSContributionSponsors > OSSContributionSponsor */
+EXEC sp_rename 'dbo.OSSContributionSponsors', 'OSSContributionSponsor'
+
+/* ProjectApprovals > RepositoryApproval */
+EXEC sp_rename 'dbo.ProjectApprovals', 'RepositoryApproval'
+
+/* Projects > Repository */
+EXEC sp_rename 'dbo.Projects', 'Repository'
+
+/* RepoTopics > RepositoryTopic */
+EXEC sp_rename 'dbo.RepoTopics', 'RepositoryTopic'
+
+/* RegionalOrganizations > RegionalOrganization */
+EXEC sp_rename 'dbo.RegionalOrganizations', 'RegionalOrganization'
+
+/* RelatedCommunities > RelatedCommunity */
+EXEC sp_rename 'dbo.RelatedCommunities', 'RelatedCommunity'
+
+/* RepoOwners > RepositoryOwner */
+EXEC sp_rename 'dbo.RepoOwners', 'RepositoryOwner'
+
+/* Users > User */
+EXEC sp_rename 'dbo.Users', 'User'
+
+/* Visibility > Visibility */
+
