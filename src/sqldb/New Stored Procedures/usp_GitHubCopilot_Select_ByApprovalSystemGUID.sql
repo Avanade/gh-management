f8bb28dcc9b9d61b67d583daaa40a6727ea8aebd
Ismael Ibuan <102030576+iibuan@users.noreply.github.com>
2024-06-06 22:46:19 +0800

    refactor: github copilot stored procedures
    Signed-off-by: Ismael Ibuan <102030576+iibuan@users.noreply.github.com>

---
+++ src/sqldb/New Stored Procedures/usp_GitHubCopilot_Select_ByApprovalSystemGUID.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE [dbo].[usp_GitHubCopilot_Select_ByApprovalSystemGUID]
+  @ApprovalSystemGUID [UNIQUEIDENTIFIER]
+AS
+BEGIN
+  SELECT 
+    [GC].[RegionalOrganizationId],
+    [RO].[Name] AS RegionName,
+    [GC].[GitHubUsername],
+    [GC].[GitHubId],
+    [GC].[Id]
+  FROM [dbo].[ApprovalRequest] AR
+  LEFT JOIN [dbo].[GitHubCopilotApprovalRequest] GCAR ON [GCAR].[ApprovalRequestId] = [AR].[Id]
+  LEFT JOIN [dbo].[GitHubCopilot] GC ON [GC].[Id] = [GCAR].[GitHubCopilotId]
+  LEFT JOIN [dbo].[RegionalOrganization] RO ON [GC].[RegionalOrganizationId] = [RO].[Id]
+  WHERE [AR].[ApprovalSystemGUID] = @ApprovalSystemGUID
+END
---
+++ src/sqldb/New Stored Procedures/usp_GitHubCopilot_Select_Pending.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE [dbo].[usp_GitHubCopilot_Select_Pending]
+    @Username [VARCHAR](100),
+    @Organization [VARCHAR](100)
+AS
+BEGIN
+  SELECT 
+    [GC].[Id],
+    [RO].[Name],
+    [GC].[GitHubUsername],
+    [GC].[Created]
+  FROM [dbo].[GitHubCopilot] GC
+  LEFT JOIN [dbo].[RegionalOrganization] RO ON [GC].[RegionalOrganizationId] = [RO].[Id]
+  LEFT JOIN [dbo].[GitHubCopilotApprovalRequest] GCAR ON [GCAR].[GitHubCopilotId] = GC.[Id]
+  LEFT JOIN [dbo].[ApprovalRequest] AR ON [AR].[Id] = [GCAR].[ApprovalRequestId]
+  WHERE 
+    [GC].[CreatedBy]=@Username AND 
+    [RO].[Name] =@Organization AND 
+    [AR].[ApprovalStatusId] < 3
+  ORDER BY [GC].[Created] DESC
+END
