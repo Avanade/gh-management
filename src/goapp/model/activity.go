a2a8d75443c6bd23418031b407479d0a69f62252
Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>
2024-08-08 23:41:41 +0800

    refactor: Add and update models
    Signed-off-by: Jerrico Dela Cruz <94591636+jerricotandelacruz@users.noreply.github.com>

--- src/goapp/model/activity.go
+++ src/goapp/model/activity.go
@@ -4,13 +4,17 @@ import "time"
 
 type Activity struct {
 	ID             int64     `json:"id"`
-	CommunityId    int64     `json:"communityId"`
 	Date           time.Time `json:"date"`
 	Name           string    `json:"name"`
-	ActivityTypeId int64     `json:"activityTypeId"`
 	Url            string    `json:"url"`
 	Created        time.Time `json:"created"`
 	CreatedBy      string    `json:"createdBy"`
 	Modified       time.Time `json:"modified"`
 	ModifiedBy     string    `json:"modifiedBy"`
+	CommunityId    int64     `json:"communityId"`
+	ActivityTypeId int64     `json:"activityTypeId"`
+
+	Community                 Community                  `json:"community"`
+	ActivityType              ActivityType               `json:"type"`
+	ActivityContributionAreas []ActivityContributionArea `json:"activityContributionAreas"`
 }
